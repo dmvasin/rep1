8aed1a527f9bcf9230cf614d13001dff2e8fd725
Dmitry <d.vasin@skiner.ru>
2017-11-01 13:17:17 +0300

    add element 5 test

--- Project5/Project5/main.cpp
+++ Project5/Project5/main.cpp
@@ -8,10 +8,10 @@ void main()
 {
 	setlocale(LC_ALL, "ru");
 
-	//тип данных [количество этементов];
+	//тип данных [количество элементов];
 
-	const int size = 5;
-	int arr[size]{ 55,511,2,123,95 };
+	const int size = 6;
+	int arr[size]{ 55,511,2,123,95,188 };
 
 	for (int i = 0; i < size; i++)
 	{
